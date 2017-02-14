e18fc8238aa1122fb9a5b070d884cb4073d19f15
erosdome <erosdome@gmail.com>
2017-02-14 14:00:42 +0100

    prov profile section in progress

---
+++ source/javascripts/factories/_ProvProfile.js
@@ -0,0 +1,15 @@
+(function() {
+
+"use strict";
+
+angular.module("BitriseWorkflowEditor").factory("ProvProfile", function() {
+
+	var ProvProfile = function() {
+		this.name;
+	};
+
+	return ProvProfile;
+
+});
+
+})();
