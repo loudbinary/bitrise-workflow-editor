bb269b69abafad4fd2f502b70de0cc4a97242405
erosdome <erosdome@gmail.com>
2017-02-15 12:41:50 +0100

    code signing ctrl added

---
+++ source/javascripts/factories/_Certificate.js
@@ -0,0 +1,16 @@
+(function() {
+
+"use strict";
+
+angular.module("BitriseWorkflowEditor").factory("Certificate", function() {
+
+	var Certificate = function() {
+		this.name;
+		this.password;
+	};
+
+	return Certificate;
+
+});
+
+})();
